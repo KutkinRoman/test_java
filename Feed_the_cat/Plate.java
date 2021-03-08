f5b89a7825b9c5bf27fefc0f74fcc960d29048d3
KRom-i <emailroma@mail.ru>
2021-03-08 08:31:08 +0300

    Home Work 7

---
+++ Feed_the_cat/Plate.java
@@ -0,0 +1,16 @@
+package Feed_the_cat;
+
+public class Plate {
+
+    public static int food;
+
+    public Plate(){}
+
+    public static void setFood(int _food){
+
+        food += _food;
+
+    }
+
+
+}
