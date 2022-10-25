a0fc42429d2a0b1a0c67f959414a292c2dabc613
R.Kutkin <R.Kutkin@ensoft.kz>
2022-10-25 20:03:15 +0600

    Test commit

--- HomeWork_1 java 3.java
+++ HomeWork_1 java 3.java
@@ -4,11 +4,15 @@ public class Fruit {
 
 public class Apple extends Fruit{
 
+
     public static float weight;
+
 
     public Apple() {
         this.weight = 1.0f;
     }
+	
+	
     public float getWeight() {
         return weight;
     }
@@ -17,11 +21,14 @@ public class Apple extends Fruit{
 
 public class Orange extends Fruit{
 
+
     private float weight;
+
 
     public Orange() {
         this.weight = 1.5f;
     }
+
 
     public float getWeight(){
         return weight;
@@ -45,7 +52,6 @@ public class BoxDemoApp {
 
     public static void main(String[] args) {
 
- 
         BoxFruit<Apple> appleBoxFruit = new BoxFruit<>();
         appleBoxFruit.add(new Apple());
         appleBoxFruit.add(new Apple());
